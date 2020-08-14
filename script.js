b98dadbe1ee9df1bcaaf8cc7b128e2fb41133d66
Brittney <brittney.postma@hotmail.com>
2020-08-13 20:05:29 -0400

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+// Unsplash API
+require('dotenv').config()
+const count = 10
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${process.env.apiKey}&count=${count}`
+
+// Get photos from Unsplash
+async function getPhotos() {
+  try {
+    const res = await fetch(apiURL)
+    const data = await res.json()
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// On Load
+getPhotos()
